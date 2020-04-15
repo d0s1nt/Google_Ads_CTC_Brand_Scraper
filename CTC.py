6afce043c70a308a7579982675d73e2fea50e414
d0s1nt <darren@darrenkingsnorth.co.uk>
2020-04-16 00:50:26 +0100

    Update CTC.py

--- CTC.py
+++ CTC.py
@@ -5,6 +5,7 @@ from collections import Counter
 from datetime import datetime
 import docx
 import io
+import ast
 
 #Insert brands
 brands = ['']
@@ -14,7 +15,7 @@ scaleserpkey = ''
 apiflashkey = ''
 
 for brand in brands:
-    excel_df = pd.read_excel('Brand_Claims.xlsx')
+    excel_df = pd.read_excel('Brand_Terms.xlsx')
     terms = excel_df.loc[:, 'Keyword']
     terms = [str(term).replace('brand', f'{brand}') for term in terms]
 
@@ -24,6 +25,7 @@ for brand in brands:
     domains_in_ads = []
     domains_in_matched_brand_ads = []
     search_blobs = []
+
 
     def submit_term(term):
         params = {
@@ -44,12 +46,14 @@ for brand in brands:
         result = api_result.json()
         return result
 
+
     def check_for_ads(serp_result):
         try:
             if serp_result['ads']:
                 contains_adverts.append('yes')
         except:
             contains_adverts.append('no')
+
 
     def ctc_check(serp_result):
         try:
@@ -65,6 +69,7 @@ for brand in brands:
         except:
             contains_ctc.append('no')
 
+
     def check_for_brand_string(serp_result):
         try:
             searchterm = [f'{brand}', f'{brand.lower()}']
@@ -77,7 +82,7 @@ for brand in brands:
                     print("----------------------------")
                     print(f"{brand} string found!")
                     print("----------------------------")
-                    print("stres:",stres)
+                    print("stres:", stres)
                     domains_in_matched_brand_ads.append(serp['domain'])
                     yeslist.append('yes')
             if yeslist:
@@ -86,6 +91,7 @@ for brand in brands:
                 contains_brand.append('no')
         except:
             contains_brand.append('no')
+
 
     def get_json_blob(serp_result):
         try:
@@ -101,7 +107,9 @@ for brand in brands:
         except:
             pass
 
-    output = {'Search Terms':terms, 'Google Ads':contains_adverts, 'Click to Call':contains_ctc, f'Contains "{brand}"':contains_brand, 'Raw SERP':search_blobs}
+
+    output = {'Search Terms': terms, 'Google Ads': contains_adverts, 'Click to Call': contains_ctc,
+              f'Contains "{brand}"': contains_brand, 'Raw SERP': search_blobs}
 
     for term in terms:
         if term:
@@ -114,39 +122,23 @@ for brand in brands:
 
     current_date = datetime.now().strftime("%d-%m-%Y")
 
-    df = pd.DataFrame(output, columns=['Search Terms', 'Google Ads', 'Click to Call', f'Contains "{brand}"', 'Raw SERP'])
-    df.to_excel(f"{brand}_CMC_{current_date}.xlsx", index=False)
-
-    urldict = {}
-
-    for index, row in df.iterrows():
-        urls = []
-        print(index, row['Search Terms'], row[f'Contains "{brand}"'], row['Raw SERP'])
-        if row[f'Contains "{brand}"'] == 'yes':
-            print(f"This row contains {brand}")
-            for serp in row['Raw SERP']:
-                try:
-                    print("displayed_link")
-                    print(serp['displayed_link'])
-                    print("domain")
-                    print(serp['domain'])
-                    urls.append(serp['displayed_link'])
-                except:
-                    pass
-        urldict[row['Search Terms']] = urls
+    df = pd.DataFrame(output,
+                      columns=['Search Terms', 'Google Ads', 'Click to Call', f'Contains "{brand}"', 'Raw SERP'])
+    df.to_excel(f"{brand}_{current_date}.xlsx", index=False)
 
     doc = docx.Document()
-    doc.add_heading(f'CMC Google results: "{brand}"', 0)
+    doc.add_heading(f'Google results: "{brand}"', 0)
     font = doc.styles['Normal'].font
     font.name = 'Calibri'
     font.size = docx.shared.Pt(11)
+
     print("Count of domains in ads: ")
     print(len(Counter(domains_in_ads)))
     print(Counter(domains_in_ads))
     count_domains_in_ads = Counter(domains_in_ads)
     print(len(count_domains_in_ads))
     r = doc.add_paragraph('Count of domains in Google Ads: ')
-    alltable = doc.add_table(rows=(len(domains_in_matched_brand_ads)), cols=2)
+    alltable = doc.add_table(rows=1, cols=2)
     alltable.style = 'Light Grid Accent 1'
     hdr_cells = alltable.rows[0].cells
     hdr_cells[0].text = 'Domain'
@@ -164,7 +156,7 @@ for brand in brands:
     print(len(count_domains_in_matched_brand_ads))
     e = doc.add_paragraph('')
     e = doc.add_paragraph('Count of domains in matched Google Ads: ')
-    matchtable = doc.add_table(rows=(len(domains_in_matched_brand_ads)), cols=2)
+    matchtable = doc.add_table(rows=1, cols=2)
     matchtable.style = 'Light Grid Accent 1'
     hdr_cells = matchtable.rows[0].cells
     hdr_cells[0].text = 'Domain'
@@ -177,25 +169,45 @@ for brand in brands:
 
     doc.add_page_break()
 
-    for term in urldict:
-        if urldict[term]:
-            p = doc.add_paragraph('Google search term: ')
-            p.add_run(str(term))
-            for url in urldict[term]:
-                print("URL is:" + url)
-                t = doc.add_paragraph('URL retrieved from Google Ad: ')
-                t.add_run(url)
-                try:
-                    try:
-                        req = f"https://api.apiflash.com/v1/urltoimage?access_key={apiflashkey}&format=png&response_type=image&url=https://{url}"
-                        url = f"https://{url}"
-                    except:
-                        req = f"https://api.apiflash.com/v1/urltoimage?access_key={apiflashkey}&format=png&response_type=image&url=http://{url}"
-                        url = f"http://{url}"
-                    response = requests.get(req, stream=True)
-                    image = io.BytesIO(response.content)
-                    doc.add_picture(image, width=docx.shared.Cm(15), height=docx.shared.Cm(7.5))
-                except:
-                    doc.add_paragraph("No image found, website not available")
-            doc.add_page_break()
-    doc.save(f'{brand}_CMC_{current_date}.docx')
+    for index, row in df.iterrows():
+        if row[f'Contains "{brand}"'] == 'yes':
+            print(row['Search Terms'])
+            p = doc.add_heading('Google search term: ', level=1)
+            p.add_run(str(row['Search Terms']))
+
+            for ser in row['Raw SERP']:
+                for key, value in ser.items():
+                    print(key, value)
+                    searchterm = [f'{brand}', f'{brand.lower()}']
+                    words_re = re.compile("|".join(searchterm))
+                    value = str(value)
+                    if words_re.search(value):
+                        found_link = (f"{brand} string found in {key}: {value}")
+                        print(found_link)
+                        print("..........................................")
+                        print(ser['domain'])
+                        domain = (f"Domain: {ser['domain']}")
+                        print("..........................................")
+                        print()
+                        doc.add_paragraph(found_link)
+                        doc.add_paragraph(domain)
+
+                        try:
+                            try:
+                                if value.startswith('htt'):
+                                    req = f"https://api.apiflash.com/v1/urltoimage?access_key={apiflashkey}&format=png&response_type=image&url={value}"
+                                else:
+                                    req = f"https://api.apiflash.com/v1/urltoimage?access_key={apiflashkey}&format=png&response_type=image&url=https://{value}"
+
+                            except:
+                                if ser['displayed_link'].startswith('htt'):
+                                    req = f"https://api.apiflash.com/v1/urltoimage?access_key={apiflashkey}&format=png&response_type=image&url={ser['displayed_link']}"
+                                else:
+                                    req = f"https://api.apiflash.com/v1/urltoimage?access_key={apiflashkey}&format=png&response_type=image&url=https://{ser['displayed_link']}"
+                            response = requests.get(req, stream=True)
+                            image = io.BytesIO(response.content)
+                            doc.add_picture(image, width=docx.shared.Cm(15), height=docx.shared.Cm(7.5))
+                            doc.add_paragraph("")
+                        except:
+                            doc.add_paragraph("No image found, website not available")
+    doc.save(f'{brand}_{current_date}.docx')
